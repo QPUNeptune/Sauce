30d09c76a052195a7c21fa150276da7a36745755
Liorlevy180 <40452442+Liorlevy180@users.noreply.github.com>
2018-12-28 16:25:20 +0200

    Add files via upload

---
+++ Sauce/Sauce/UI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sauce
+{
+
+    public partial class UI : Form
+    {
+        public UI()
+        {
+            InitializeComponent();
+        }
+        private void InitializeMyControl()
+        {
+            // Put some text into the control first.  
+            textBox2.Text = "This is a TextBox control.";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string var;
+            var = textBox2.Text;
+            MessageBox.Show(var);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string var;
+            var = textBox2.Text;
+  
+                System.Diagnostics.Process.Start("https://nhentai.net/g/" + var);
+            
+            
+            
+        }
+
+        private void UI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
